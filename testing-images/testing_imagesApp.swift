f2798d47cad4ac5102fd72aed5e53539e207ca6f
Alex little <alex@reelapp.co.uk>
2022-08-23 17:48:03 +0100

    Initial Commit

---
+++ testing-images/testing_imagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_imagesApp.swift
+//  testing-images
+//
+//  Created by Alex Little on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct testing_imagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
